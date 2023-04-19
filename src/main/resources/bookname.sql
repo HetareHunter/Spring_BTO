e270688dc9837c5817ab9e58497983819f444466
desk_katsu <dai1nokira@gmail.com>
2023-04-20 06:15:26 +0900

    本の貸し出しページのレイアウトを修正、文字がはみ出ないようにした。カードの幅を調整、文字の改行が反映されるようにした

--- src/main/resources/bookname.sql
+++ src/main/resources/bookname.sql
@@ -8,7 +8,8 @@ INSERT INTO BOOK_NAMES(ID,ACTIVE,AUTHOR,CREATED_AT,DETAIL,IMG,PUBLISHER,TITLE,UP
     TRUE,
     '中山清喬, 国本大悟',
     CURRENT_TIMESTAMP,
-    '本書ではJavaでドラクエ風RPGの制作することをテーマとすることで、読者に楽しそうなイメージを持ってもらい、途中で挫折せずにJavaを学べるよう配慮すると共に、肝心のオブジェクト指向についてもRPG風のイラストを多用して直感的に分かりやすく理解できるよう工夫しました。',
+    '本書ではJavaでドラクエ風RPGの制作することをテーマとすることで、
+    読者に楽しそうなイメージを持ってもらい、途中で挫折せずにJavaを学べるよう配慮すると共に、肝心のオブジェクト指向についてもRPG風のイラストを多用して直感的に分かりやすく理解できるよう工夫しました。',
     'Javasukkiri.png',
     'インプレス',
     'スッキリわかるJava入門',
@@ -19,7 +20,8 @@ INSERT INTO BOOK_NAMES(ID,ACTIVE,AUTHOR,CREATED_AT,DETAIL,IMG,PUBLISHER,TITLE,UP
     TRUE,
     '山田 祥寛',
     CURRENT_TIMESTAMP,
-    '標準教科書”が完全書き下ろしで11年ぶり新登場！Javaプログラミングに必要な知識・概念・機能を体系的かつ網羅的に習得！',
+    '標準教科書”が完全書き下ろしで11年ぶり新登場！
+    Javaプログラミングに必要な知識・概念・機能を体系的かつ網羅的に習得！',
     'dokusyuJava.jpg',
     '翔泳社',
     '独習Java 新版',
@@ -30,10 +32,11 @@ INSERT INTO BOOK_NAMES(ID,ACTIVE,AUTHOR,CREATED_AT,DETAIL,IMG,PUBLISHER,TITLE,UP
     TRUE,
     '株式会社NTTデータ',
     CURRENT_TIMESTAMP,
-    'Spring Frameworkの基礎から開発時の指針まで！定番OSSフレームワークによるJavaシステム開発の入門書！',
+    'Spring Frameworkの基礎から開発時の指針まで！
+    定番OSSフレームワークによるJavaシステム開発の入門書！',
     'springbootettei.jpg',
     '翔泳社',
-    'Spring徹底入門 Spring FrameworkによるJavaアプリケーション開発',
+    'Spring徹底入門',
     CURRENT_TIMESTAMP,
     2),
     (
@@ -45,7 +48,7 @@ INSERT INTO BOOK_NAMES(ID,ACTIVE,AUTHOR,CREATED_AT,DETAIL,IMG,PUBLISHER,TITLE,UP
     Spring Bootアプリケーションをいくつか作成しながら、主要な機能について解説しています。ご興味がありましたら、是非一読ください。',
     'springboot2nyumon.jpg',
     'Independently published',
-    'Spring Boot 2 入門: 基礎から実演まで',
+    'Spring Boot 2 入門',
     CURRENT_TIMESTAMP,
     2),
     (
@@ -57,6 +60,7 @@ INSERT INTO BOOK_NAMES(ID,ACTIVE,AUTHOR,CREATED_AT,DETAIL,IMG,PUBLISHER,TITLE,UP
 新しい試験範囲を完全に網羅。教科書テキストがなくても、この問題集1冊でとても丁寧に解説しているので、ラムダ式やモジュールについてもすんなりと理解できます。',
     'javasilverse11.jpg',
     'インプレス',
-    '徹底攻略Java SE 11 Silver問題集[1Z0-815]対応',
+    '徹底攻略Java SE 11 Silver問題集
+    [1Z0-815]対応',
     CURRENT_TIMESTAMP,
     2);
